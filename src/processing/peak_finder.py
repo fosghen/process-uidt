2340bd6fecd3de52ada7aa633575e75f2041cc1c
fosghen <r.efremov2@g.nsu.ru>
2025-07-01 12:12:25 +0700

    add get laplace func for eval approx

--- src/processing/peak_finder.py
+++ src/processing/peak_finder.py
@@ -10,13 +10,13 @@ class PeakFinder:
         self.data_type = data_type
 
     @staticmethod
-    def _laplace_func(x: np.ndarray, mu: float, b: float) -> np.ndarray:
-        return np.exp(-abs(x - mu) / b)
+    def _laplace_func(x: np.ndarray, mu: float, b: float, a: float) -> np.ndarray:
+        return a * np.exp(-abs(x - mu) / b)
     
     @staticmethod
     def _get_peaks(data: np.ndarray) -> tuple[np.ndarray, np.ndarray]:
         image = data.T
-        kernel = PeakFinder._laplace_func(np.arange(10), 5, 5)
+        kernel = PeakFinder._laplace_func(np.arange(10), 5, 5, 1)
         # "Сглаживаем" сигнал с помощью функции Лапласа
         result = convolve1d(image, kernel, axis=0, mode='constant')
         
@@ -41,16 +41,16 @@ class PeakFinder:
     def _process_row_analyze(freq: np.ndarray, data_row: np.ndarray, *args) -> float:
         try:
             # ищем точку в которой пик похож на тот, что мы ожидаем
-            index_max = np.argmax(np.correlate(data_row, PeakFinder._laplace_func(np.arange(10), 5, 5), mode='full')) - 5
+            index_max = np.argmax(np.correlate(data_row, PeakFinder._laplace_func(np.arange(10), 5, 5, 1), mode='full')) - 5
             # сглаживаем 
             y_smooth = medfilt(data_row, kernel_size=5)
             params, _ = curve_fit(PeakFinder._laplace_func,
                                   freq,
                                   y_smooth,
-                                  p0=[freq[index_max], 3],
+                                  p0=[freq[index_max], 10, 1],
                                   maxfev=1000,
-                                  bounds=([freq[0],     2],
-                                          [freq[-1],    4]),
+                                  bounds=([freq[0],     1,  0.5],
+                                          [freq[-1],    100, 1.5]),
                                           )
             return params[0]
         except Exception:
@@ -60,24 +60,25 @@ class PeakFinder:
     def _process_row_reflectometer(freq: np.ndarray, data_row: np.ndarray,
                                    i: int, lo_peaks: np.ndarray, hi_peaks: np.ndarray) -> float:
         try:
+            # аппроксимация левого пика
             params0, _ = curve_fit(PeakFinder._laplace_func, 
                                 freq[:lo_peaks[i]], 
                                 data_row[:lo_peaks[i]], 
-                                p0=[freq[min(hi_peaks[i])], 3],
+                                p0=[freq[min(hi_peaks[i])], 10, 1],
                                 maxfev=100,
-                                bounds=([freq[0],          2],
-                                        [freq[lo_peaks[i]], 4]))
-                
+                                bounds=([freq[0],           1, 0.5],
+                                        [freq[lo_peaks[i]], 100, 1.5]))
+            # аппроксимация правого пика    
             params1, _ = curve_fit(PeakFinder._laplace_func,
                                 freq[lo_peaks[i]:],
                                 data_row[lo_peaks[i]:],
-                                p0=[freq[max(hi_peaks[i])], 3],
+                                p0=[freq[max(hi_peaks[i])], 10, 1],
                                 maxfev=100,
-                                bounds=((freq[lo_peaks[i]], 2),
-                                        (freq[-1],         4)))
+                                bounds=((freq[lo_peaks[i]], 1, 0.5),
+                                        (freq[-1],          100, 1.5)))
             return (params0[0] + params1[0]) / 2
         except Exception:
-            
+            #Если что-то идёт не так, то берём среднее значение
             return float(freq[hi_peaks[i][0]] + freq[hi_peaks[i][1]]) / 2
 
     @staticmethod
@@ -92,4 +93,46 @@ class PeakFinder:
             lo_peaks, hi_peaks = PeakFinder._get_peaks(data)
             args = (lo_peaks, hi_peaks)
         return PeakFinder._find_peak_parallel(freq, finder, data, *args)
+    
+    def get_approx_laplace(self, freq: np.ndarray, data: np.ndarray) -> np.ndarray:
+        """Функция, выдающая аппроксимирующие функции Лапласса для набора данных."""
+        approx_laplace = np.zeros((3, freq.size))
+        
+        for i, data_row in enumerate(data):
+            if self.data_type == "refl":
+                # находим предположительные пики 
+                lo_peaks, hi_peaks = PeakFinder._get_peaks(data)
+                # аппроксимируем левый пик
+                params0, _ = curve_fit(PeakFinder._laplace_func, 
+                                    freq[:lo_peaks[i]], 
+                                    data_row[:lo_peaks[i]], 
+                                    p0=[freq[min(hi_peaks[i])], 10, 1],
+                                    maxfev=100,
+                                    bounds=([freq[0],           1, 0.5],
+                                            [freq[lo_peaks[i]], 100, 1.5]))
+                approx_laplace[i, :lo_peaks[i]] = PeakFinder._laplace_func(freq[:lo_peaks[i]], *params0)
+                # аппроксимируем правый пик
+                params1, _ = curve_fit(PeakFinder._laplace_func,
+                                    freq[lo_peaks[i]:],
+                                    data_row[lo_peaks[i]:],
+                                    p0=[freq[max(hi_peaks[i])], 10, 1],
+                                    maxfev=100,
+                                    bounds=((freq[lo_peaks[i]], 1, 0.5),
+                                            (freq[-1],         100, 1.5)))
+                approx_laplace[i, lo_peaks[i]:] = PeakFinder._laplace_func(freq[lo_peaks[i]:], *params1)
+                
+            else:
+                index_max = np.argmax(np.correlate(data_row, PeakFinder._laplace_func(np.arange(10), 5, 5, 1), mode='full')) - 5
+                y_smooth = medfilt(data_row, kernel_size=5)
+                params, _ = curve_fit(PeakFinder._laplace_func,
+                                    freq,
+                                    y_smooth,
+                                    p0=[freq[index_max], 10, 1],
+                                    maxfev=1000,
+                                    bounds=([freq[0],     1, 0.5],
+                                            [freq[-1],    100, 1.5]),
+                                            )
+                approx_laplace[i] = PeakFinder._laplace_func(freq, *params)
+
+        return approx_laplace
     
